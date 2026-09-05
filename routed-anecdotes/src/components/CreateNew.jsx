f2c0ccb37679560952e1a9690a2967dd654623eb
Janne Kärki <janne.karki@helsinki.fi>
2026-09-05 20:27:05 +0300

    7.4-7.6 useAnecdotes done

--- routed-anecdotes/src/components/CreateNew.jsx
+++ routed-anecdotes/src/components/CreateNew.jsx
@@ -1,15 +1,16 @@
 import { useNavigate } from "react-router-dom"
-import { useField } from "../hooks"
+import { useAnecdotes, useField } from "../hooks"
 
-const CreateNew = ({ addNew }) => {
+const CreateNew = () => {
+  const { addAnecdote } = useAnecdotes()
   const { reset: resetContent, ...content } = useField("text")
   const { reset: resetAuthor, ...author } = useField("text")
   const { reset: resetInfo, ...info } = useField("text")
   const navigate = useNavigate()
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
-    addNew({
+    await addAnecdote({
       content: content.value,
       author: author.value,
       info: info.value,
--- routed-anecdotes/src/hooks/index.js
+++ routed-anecdotes/src/hooks/index.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import anecdoteService from '../services/anecdotes'
 
 export const useField = (type) => {
   const [value, setValue] = useState('')
@@ -17,4 +18,24 @@ export const useField = (type) => {
     onChange,
     reset
   }
+}
+
+export const useAnecdotes = () => {
+  const [anecdotes, setAnecdotes] = useState([])
+
+  useEffect(() => {
+    anecdoteService.getAll().then(data => setAnecdotes(data))
+  }, [])
+
+  const addAnecdote = async (anecdote) => {
+    const newAnecdote = await anecdoteService.createNew(anecdote)
+    setAnecdotes(anecdotes.concat(newAnecdote))
+  }
+
+  const deleteAnecdote = async (id) => {
+    await anecdoteService.remove(id)
+    setAnecdotes(anecdotes.filter(anecdote => anecdote.id !== id))
+  }
+
+  return { anecdotes, addAnecdote, deleteAnecdote }
 }
